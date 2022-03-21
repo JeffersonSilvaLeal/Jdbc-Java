a3dbcb4412eab8526db7ab6ff82e31b7823ce76f
JeffersonSilva <drankhar@gmail.com>
2022-03-21 16:42:16 -0300

    commit Criando nossa consulta SQL ao banco de dados

--- src/main/java/dao/UserJdbcDao.java
+++ src/main/java/dao/UserJdbcDao.java
@@ -2,43 +2,109 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-
-import javax.swing.text.DefaultEditorKit.InsertBreakAction;
+import java.util.ArrayList;
+import java.util.List;
 
 import conexao.jdbc.SingleConnection;
 import model.UserJdbcJava;
 
 public class UserJdbcDao {
 
-	
 	private Connection connection;
-	
+
 	public UserJdbcDao() {
 		// Instância a conexão
 		connection = SingleConnection.getConnection();
 	}
-	
-	
+
 	public void salvar(UserJdbcJava userJdbcJava) {
-	
+
 		try {
-			
+
 			// Sql
 			String sql = "insert into userjdbcjava (id, nome, email) values (?,?,?)";
 			// Prepara ele
 			PreparedStatement insert = connection.prepareStatement(sql);
 			// Dados
 			insert.setLong(1, userJdbcJava.getId());
-			insert.setString(2,	userJdbcJava.getNome());
+			insert.setString(2, userJdbcJava.getNome());
 			insert.setString(3, userJdbcJava.getEmail());
 			// Executa o insert
 			insert.execute();
 			// Salva no banco
 			connection.commit();
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+	}
+
+	public List<UserJdbcJava> listar() {
+		// Instância a lista
+		List<UserJdbcJava> list = new ArrayList<UserJdbcJava>();
+
+		// Sql 
+		String sql = "select * from userjdbcjava";
+
+		try {
+			// Prepara o sql
+			PreparedStatement statement = connection.prepareStatement(sql);
+			// Executa no banco de dados
+			ResultSet resultado = statement.executeQuery();
+
+			// Itera no banco enquanto for true
+			while (resultado.next()) {
+				// Cria os objetos
+				UserJdbcJava userJdbcJava = new UserJdbcJava();
+				// Seta os atributos
+				userJdbcJava.setId(resultado.getLong("id"));
+				userJdbcJava.setNome(resultado.getString("nome"));
+				userJdbcJava.setEmail(resultado.getString("email"));
+				
+				// Adiciona a lista
+				list.add(userJdbcJava);
+			}
+			// trata os erros
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		// retorna a lista
+		return list;
+	}
+	
+	
+	
+	public UserJdbcJava buscar(Long id) {
+		UserJdbcJava retorno = new UserJdbcJava();
+
+		// Sql 
+		String sql = "select * from userjdbcjava where id = " + id;
+
+		try {
+			// Prepara o sql
+			PreparedStatement statement = connection.prepareStatement(sql);
+			// Executa no banco de dados
+			ResultSet resultado = statement.executeQuery();
+
+			// Itera no banco enquanto for true
+			while (resultado.next()) {
+				// Cria os objetos
+				UserJdbcJava userJdbcJava = new UserJdbcJava();
+				// Seta os atributos
+				retorno.setId(resultado.getLong("id"));
+				retorno.setNome(resultado.getString("nome"));
+				retorno.setEmail(resultado.getString("email"));
+				
+			}
+			// trata os erros
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		// retorna a lista
+		return retorno;
 	}
 }
--- src/test/java/jdbc_java/jdbc_java/TesteJdbcJava.java
+++ src/test/java/jdbc_java/jdbc_java/TesteJdbcJava.java
@@ -1,5 +1,8 @@
 package jdbc_java.jdbc_java;
 
+import java.nio.file.spi.FileSystemProvider;
+import java.util.List;
+
 import org.junit.Test;
 
 import dao.UserJdbcDao;
@@ -9,14 +12,49 @@ public class TesteJdbcJava {
 
 	// testando conexão do banco
 	@Test
-	public void initBanco() {
+	public void insert() {
 		UserJdbcDao userJdbcDao = new UserJdbcDao();
 		UserJdbcJava userJdbcJava = new UserJdbcJava();
-		
+
 		userJdbcJava.setId(6L);
 		userJdbcJava.setNome("ueldon");
 		userJdbcJava.setEmail("Ueldon@gmail.com");
 
 		userJdbcDao.salvar(userJdbcJava);
 	}
+
+	@Test
+	public void Listar() {
+
+		try {
+			UserJdbcDao dao = new UserJdbcDao();
+			List<UserJdbcJava> list = dao.listar();
+			
+			for (UserJdbcJava userJdbcJava : list) {
+				System.out.println(userJdbcJava);
+				System.out.println("----------");
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	@Test
+	public void buscar() {
+		
+		UserJdbcDao dao = new UserJdbcDao();
+		try {
+			UserJdbcJava userJdbcJava = dao.buscar(6L);
+			System.out.println(userJdbcJava);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+
 }
