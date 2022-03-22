f5eb35a8c5ed12ebc037efae19f2fefb22cc24fd
JeffersonSilva <drankhar@gmail.com>
2022-03-22 12:16:51 -0300

    commit Criando tabelas de telefones no banco de dados

--- src/test/java/jdbc_java/jdbc_java/sql.sql
+++ src/test/java/jdbc_java/jdbc_java/sql.sql
@@ -23,3 +23,35 @@ select * from userjdbcjava
 
 /*Implementando a sequencia na coluna id*/
 alter table userjdbcjava ALTER column id set default nextval('usersequence'::regclass);
+
+/* Cria um Id unico*/
+alter table userjdbcjava add unique(id);
+
+/* criando a tabela telefone*/
+create table telefoneuser
+(
+    id bigint not null,
+    numero character varying (255) not null,
+	tipo character varying (255) not null,
+	userpessoa bigint not null,
+	constraint telefone_id primary key (id)
+)
+
+/* Referenciando telefoneuser para pessoasuser*/
+  alter table telefoneuser add foreign key (userpessoa) references userjdbcjava(id)
+  
+  /* Sequenciação da tabela telefoneuser*/
+  
+CREATE SEQUENCE IF NOT EXISTS public.user_telefone_seq
+    INCREMENT 1
+    START 7
+    MINVALUE 1
+    MAXVALUE 9223372036854775807
+    CACHE 1;
+
+ALTER SEQUENCE public.user_telefone_seq
+    OWNER TO postgres;
+    
+    /* Implementando sequencia no id telefoneuser*/
+    ALTER TABLE IF EXISTS public.telefoneuser
+   ALTER column id SET DEFAULT nextval('user_telefone_seq'::regclass);
